b5f3a6c4ab0e531571c583aa4c3ea8873032dffa
Raj-Debug <67727234+Raj-Debug@users.noreply.github.com>
2026-06-20 21:57:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://iridescent-maamoul-ab0d48.netlify.app/"
+    "https://iridescent-maamoul-ab0d48.netlify.app"
   ],
   credentials: true
 }));
